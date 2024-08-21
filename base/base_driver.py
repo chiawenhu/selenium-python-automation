f5f53ba510e64365d61f06032a38201079e32167
chiawen.hu11@gmail.com <chiawen.hu11@gmail.com>
2024-08-21 14:01:21 -0400

    commit for second time

--- base/base_driver.py
+++ base/base_driver.py
@@ -35,3 +35,5 @@ class BaseDriver():
         wait = WebDriverWait(self.driver, 10)
         element = wait.until(EC.element_to_be_clickable((locator_type, locator)))
         return element
+    
+    #lines to be added 8/21
